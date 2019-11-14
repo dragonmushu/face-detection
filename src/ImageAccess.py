0efcf0e14329533025263088fe344a7563faea88
Akshay Ben <akshay.ben.n@gmail.com>
2019-11-14 12:47:05 -0500

    Fixed ImageAccess class

--- src/ImageAccess.py
+++ src/ImageAccess.py
@@ -1,10 +1,10 @@
 import cv2
 import numpy as np
 import pandas as pd
-import os
-import copy
 
-PATH_TO_IMAGES = "..\images\"
+import os
+import sys
+import copy
 
 class ImageAccess:
     """
@@ -18,7 +18,7 @@ class ImageAccess:
         _images: pandas dataframe containing image and descriptors
     """
 
-     # singl   
+     # singleton static instance   
     _instance = None
 
     # image type constants
@@ -37,8 +37,8 @@ class ImageAccess:
     # image angle constants
     ALL_DEGREES = 0
     DEG_0 = 1
-    DEG_30 = 1
-    DEG_45 = 2
+    DEG_30 = 2
+    DEG_45 = 3
 
     # image orientation constants
     ALL_ORIENTATIONS = 0
@@ -51,92 +51,100 @@ class ImageAccess:
 
     def __init__(self):
         if ImageAccess._instance != None:
-            raise Exception('Singleton. Please use get_instance function.')
+            raise Exception('Singleton. Please do not directly create instance.')
         else:    
             ImageAccess._instance = self
 
-    def _type(filename):
-        if filename.find("test"):
-            return TEST
-        if filename.find("train"):
-            return TRAIN
+    def _type(self, filename):
+        if filename.find("test") != -1:
+            return self.TEST
+        if filename.find("train") != -1:
+            return self.TRAIN
         return -1
 
-    def _person(filename):
+    def _person(self, filename):
         if filename.find("akshay") != -1:
-            return AKSHAY
+            return self.AKSHAY
         if filename.find("angela") != -1:
-            return ANGELA
+            return self.ANGELA
         if filename.find("mark") != -1:
-            return MARK
+            return self.MARK
         if filename.find("isaac") != -1:
-            return ISAAC
+            return self.ISAAC
         if filename.find("nabilah") != -1:
-            return NABILAH
+            return self.NABILAH
         return -1
     
-    def _degrees(filename):
+    def _degrees(self, filename):
         if filename.find("_0_") != -1:
-            return DEG_0
+            return self.DEG_0
         if filename.find("_30") != -1:
-            return DEG_30
+            return self.DEG_30
         if filename.find("_45") != -1:
-            return DEG_45
+            return self.DEG_45
         return -1
 
-    def _orientation(filename):
+    def _orientation(self, filename):
         if filename.find("l_"):
-            return LEFT
+            return self.LEFT
         if filename.find("r_"):
-            return RIGHT
+            return self.RIGHT
 
     def initialize(self):
-        self._images = pd.DataFrame(columns=('Type', 'Person', 'Degree', 'Orientation', 'RGB', 'Grayscale'))
+        # obtain filenames from images directory
+        dirname = os.path.join(os.path.dirname(os.getcwd()), 'images\\')
+        all_files = os.listdir(dirname)
+        all_image_files = [name for name in all_files if name.endswith('.jpg')]
         
         # Fastest way to initialize pandas dataframe to create rows
         # https://stackoverflow.com/questions/10715965/add-one-row-to-pandas-dataframe
-        all_filenames = os.listdir(PATH_TO_IMAGES)
         all_rows = []
-        for filename in all_filenames:
+        for filename in all_image_files:
+            filename = os.path.join(dirname, filename)
+
             image_dict = {}
 
-            image_dict['Type'] = _type(filename)
-            image_dict['Person'] = _person(filename)
-            image_dict['Degree'] = _degrees(filename)
-            image_dict['Orientation'] = _orientation(filename)
+            image_dict['Type'] = self._type(filename)
+            image_dict['Person'] = self._person(filename)
+            image_dict['Degree'] = self._degrees(filename)
+            image_dict['Orientation'] = self._orientation(filename)
+            image_dict['Filename'] = filename
             image = cv2.imread(filename)
-            image_dict['RGB'] = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             image_dict['Grayscale'] = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             all_rows.append(image_dict)
 
-        self._images = pd.DataFrame(all_rows, columns=('Type', 'Person', 'Degree', 'Orientation', 'RGB', 'Grayscale'))
+        self._images = pd.DataFrame(all_rows, columns=('Type', 'Person', 'Degree', 'Orientation', 'Filename', 'Grayscale'))
 
     @staticmethod
     def  _get_instance():
         if ImageAccess._instance == None:
             ImageAccess._instance = ImageAccess()
-            ImageAccess._instance.initialiaze()
+            ImageAccess._instance.initialize()
 
         return ImageAccess._instance  
 
     @staticmethod
-    def obtain_images(i_type=TRAIN, i_person=ALL_PERSONS, i_degrees=ALL_DEGREES, i_orientation=ALL_ORIENTATIONS, i_color=RGB):
+    def obtain_images(i_type=1, i_person=0, i_degrees=0, i_orientation=0, i_color=1):
         instance = ImageAccess._get_instance()
         current_df = instance._images
 
-        if i_type != ALL_TYPE:
+        if i_type != ImageAccess.ALL_TYPE:
             current_df = current_df.loc[current_df['Type'] == i_type]
-        if i_person != ALL_PERSONS:
+        if i_person != ImageAccess.ALL_PERSONS:
             current_df = current_df.loc[current_df['Person'] == i_person]
-        if i_degrees != ALL_DEGREES:
+        if i_degrees != ImageAccess.ALL_DEGREES:
             current_df = current_df.loc[current_df['Degree'] == i_degrees]
-        if i_orientation != ALL_ORIENTATIONS:
+        if i_orientation != ImageAccess.ALL_ORIENTATIONS:
             current_df = current_df.loc[current_df['Orientation'] == i_orientation]
+        
+        pd.set_option('max_colwidth', 1000)
+        print(current_df['Filename'])
 
         images = []
-        if i_color == RGB:
-            images = current_df['RGB'].to_numpy()
+        if i_color == ImageAccess.RGB:
+            filenames = current_df['Filename'].to_list()
+            images = [cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB) for filename in filenames]
+            return images
         else:
             images = current_df['Grayscale'].to_numpy()
-        
-        return copy.deepcopy(images)
+            return copy.deepcopy(images)
