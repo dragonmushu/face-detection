462ab5061ed145c81782831226dba36e8a7ae2ce
Akshay Ben <akshay.ben.n@gmail.com>
2019-11-17 21:05:59 -0500

    Akshayben/pre process (#9)
    - Changed image_access and created evaluation
    - Fixed Bugs.

--- src/image_access.py
+++ src/image_access.py
@@ -1,9 +1,83 @@
 import cv2
 import numpy as np
 import pandas as pd
+import pickle
 
 import os
 import copy
+
+from helper import *
+
+# image size
+IMAGE_COMPRESSION_SIZE = 256
+
+def type_from_filename(filename):
+    if filename.find("test") != -1:
+        return DataType.TEST
+    if filename.find("train") != -1:
+        return DataType.TRAIN
+    return -1
+
+def person_from_filename(filename):
+    if filename.find("akshay") != -1:
+        return Label.AKSHAY
+    if filename.find("angela") != -1:
+        return Label.ANGELA
+    if filename.find("mark") != -1:
+        return Label.MARK
+    if filename.find("isaac") != -1:
+        return Label.ISAAC
+    if filename.find("nabilah") != -1:
+        return Label.NABILAH
+    return -1
+
+def degrees_from_filename(filename):
+    if filename.find("_0_") != -1:
+        return Angle.DEG_0
+    if filename.find("_30") != -1:
+        return Angle.DEG_30
+    if filename.find("_45") != -1:
+        return Angle.DEG_45
+    return -1
+
+def orientation_from_filename(filename):
+    if filename.find("l_") != -1:
+        return Orientation.LEFT
+    if filename.find("r_") != -1:
+        return Orientation.RIGHT
+
+def preprocess_images(images_directory):
+    # obtain filenames from images directory
+    all_files = os.listdir(images_directory)
+    all_image_files = [name for name in all_files if name.endswith('.jpg')]
+    
+    # Fastest way to initialize pandas dataframe to create rows
+    # https://stackoverflow.com/questions/10715965/add-one-row-to-pandas-dataframe
+    all_rows = []
+    for filename in all_image_files:
+        filename = os.path.join(images_directory, filename)
+
+        image_dict = {}
+
+        image_dict['Type'] = type_from_filename(filename)
+        image_dict['Person'] = person_from_filename(filename)
+        image_dict['Degree'] = degrees_from_filename(filename)
+        image_dict['Orientation'] = orientation_from_filename(filename)
+        image_dict['Filename'] = filename
+
+        image = cv2.imread(filename)
+        image = cv2.resize(image, (IMAGE_COMPRESSION_SIZE, IMAGE_COMPRESSION_SIZE))
+        image_dict['RGB'] = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image_dict['Grayscale'] = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        all_rows.append(image_dict)
+
+    # create dataframe of images
+    images = pd.DataFrame(all_rows, columns=('Type', 'Person', 'Degree', 'Orientation', 'Filename', 'Grayscale', 'RGB'))
+
+    # pickle and save images
+    images.to_pickle('data.pkl')
+
+    return images
 
 class ImageAccess:
     """
@@ -20,114 +94,45 @@ class ImageAccess:
      # singleton static instance   
     _instance = None
 
-    # image size
-    IMAGE_SIZE = 256
-
-    # image type constants
-    ALL_TYPE = 0
-    TRAIN = 1
-    TEST = 2
-
-    # image people constants
-    ALL_PERSONS = 0
-    AKSHAY = 1
-    ANGELA = 2
-    ISAAC = 3
-    MARK = 4
-    NABILAH = 5
-
-    # image angle constants
-    ALL_DEGREES = 0
-    DEG_0 = 1
-    DEG_30 = 2
-    DEG_45 = 3
-
-    # image orientation constants
-    ALL_ORIENTATIONS = 0
-    LEFT = 1
-    RIGHT = 2
-
-    # image color constants
-    RGB = 0
-    GRAYSCALE = 1
-
     def __init__(self):
         if ImageAccess._instance != None:
             raise Exception('Singleton. Please do not directly create instance.')
         else:    
             ImageAccess._instance = self
 
-    def _type(self, filename):
-        if filename.find("test") != -1:
-            return self.TEST
-        if filename.find("train") != -1:
-            return self.TRAIN
-        return -1
-
-    def _person(self, filename):
-        if filename.find("akshay") != -1:
-            return self.AKSHAY
-        if filename.find("angela") != -1:
-            return self.ANGELA
-        if filename.find("mark") != -1:
-            return self.MARK
-        if filename.find("isaac") != -1:
-            return self.ISAAC
-        if filename.find("nabilah") != -1:
-            return self.NABILAH
-        return -1
-    
-    def _degrees(self, filename):
-        if filename.find("_0_") != -1:
-            return self.DEG_0
-        if filename.find("_30") != -1:
-            return self.DEG_30
-        if filename.find("_45") != -1:
-            return self.DEG_45
-        return -1
-
-    def _orientation(self, filename):
-        if filename.find("l_") != -1:
-            return self.LEFT
-        if filename.find("r_") != -1:
-            return self.RIGHT
-
     def initialize(self):
-        # obtain filenames from images directory
-        dirname = os.path.join(os.path.dirname(os.getcwd()), 'images')
-        all_files = os.listdir(dirname)
-        all_image_files = [name for name in all_files if name.endswith('.jpg')]
+        if os.path.exists('data.pkl'):
+            self._images = df = pd.read_pickle('data.pkl')
+        else:
+            dirname = os.path.join(os.path.dirname(os.getcwd()), 'images')
+            self._images = preprocess_images(dirname)
         
-        # Fastest way to initialize pandas dataframe to create rows
-        # https://stackoverflow.com/questions/10715965/add-one-row-to-pandas-dataframe
-        all_rows = []
-        for filename in all_image_files:
-            filename = os.path.join(dirname, filename)
-
-            image_dict = {}
-
-            image_dict['Type'] = self._type(filename)
-            image_dict['Person'] = self._person(filename)
-            image_dict['Degree'] = self._degrees(filename)
-            image_dict['Orientation'] = self._orientation(filename)
-            image_dict['Filename'] = filename
-            image = cv2.imread(filename)
-            image = cv2.resize(image, (ImageAccess.IMAGE_SIZE, ImageAccess.IMAGE_SIZE))
-            image_dict['Grayscale'] = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-            all_rows.append(image_dict)
-
-        self._images = pd.DataFrame(all_rows, columns=('Type', 'Person', 'Degree', 'Orientation', 'Filename', 'Grayscale'))
-
     @staticmethod
     def  _get_instance():
         if ImageAccess._instance == None:
             ImageAccess._instance = ImageAccess()
             ImageAccess._instance.initialize()
 
-        return ImageAccess._instance  
+        return ImageAccess._instance
 
     @staticmethod
-    def obtain_images(i_type=1, i_person=0, i_degrees=0, i_orientation=0, i_color=1):
+    def _filter_df(i_type, i_person, i_degrees, i_orientation, i_color):
+        instance = ImageAccess._get_instance()
+        current_df = instance._images
+
+        if i_type != DataType.ALL_TYPE:
+            current_df = current_df.loc[current_df['Type'] == i_type]
+        if i_person != Label.ALL_PERSONS:
+            current_df = current_df.loc[current_df['Person'] == i_person]
+        if i_degrees != Angle.ALL_DEGREES:
+            current_df = current_df.loc[current_df['Degree'] == i_degrees]
+        if i_orientation != Orientation.ALL_ORIENTATIONS:
+            current_df = current_df.loc[current_df['Orientation'] == i_orientation]
+        
+        return current_df
+
+    @staticmethod
+    def obtain_images(i_type=DataType.TRAIN, i_person=Label.ALL_PERSONS, i_degrees=Angle.ALL_DEGREES, i_orientation=Orientation.ALL_ORIENTATIONS, i_color=Color.GRAYSCALE):
         """
         Function to obtain images you want to use for processing or testing 
 
@@ -135,33 +140,52 @@ class ImageAccess:
             if obtaining rgb images be careful not to choose too many so you don't run out of memory
 
         Args:
-            i_type: type of image (see above, could be train or test). Default set to ImageAccess.ALL_TYPES
-            i_person: person you want (see above). Default set to ImageAccess.ALL_PERSONS
-            i_degrees: image angle desired (see above, could be 0, 30, 45). Default set to ImageAccess.ALL_DEGREES
-            i_orientation: desired orientation (see above, could be left or right). Default set to ImageAccess.ALL_PERSONS
-            i_color: image color scheme (see above). Default set to ImageAccess.Grayscale
+            i_type: type of image (see helper.py). Default set to ImageAccess.ALL_TYPES
+            i_person: person you want (see helper.py). Default set to ImageAccess.ALL_PERSONS
+            i_degrees: image angle desired (see helper.py). Default set to ImageAccess.ALL_DEGREES
+            i_orientation: desired orientation (see helper.py). Default set to ImageAccess.ALL_PERSONS
+            i_color: image color scheme (see helper.py). Default set to ImageAccess.Grayscale
 
         Returns:
             np array of images filtered from data frame
         """
-        
-        instance = ImageAccess._get_instance()
-        current_df = instance._images
 
-        if i_type != ImageAccess.ALL_TYPE:
-            current_df = current_df.loc[current_df['Type'] == i_type]
-        if i_person != ImageAccess.ALL_PERSONS:
-            current_df = current_df.loc[current_df['Person'] == i_person]
-        if i_degrees != ImageAccess.ALL_DEGREES:
-            current_df = current_df.loc[current_df['Degree'] == i_degrees]
-        if i_orientation != ImageAccess.ALL_ORIENTATIONS:
-            current_df = current_df.loc[current_df['Orientation'] == i_orientation]
+        current_df = ImageAccess._filter_df(i_type, i_person, i_degrees, i_orientation, i_color)
 
-        images = []
-        if i_color == ImageAccess.RGB:
-            filenames = current_df['Filename'].to_list()
-            images = [cv2.cvtColor(cv2.imread(filename), cv2.COLOR_BGR2RGB) for filename in filenames]
-            return np.array(images)
+        if i_color == Color.RGB:
+            images = current_df['RGB'].to_numpy()
+            return images
         else:
             images = current_df['Grayscale'].to_numpy()
-            return copy.deepcopy(images)
+            return images
+
+    @staticmethod
+    def obtain_labeled_data(i_type=DataType.TRAIN, i_person=Label.ALL_PERSONS, i_degrees=Angle.ALL_DEGREES, i_orientation=Orientation.ALL_ORIENTATIONS, i_color=Color.GRAYSCALE):
+        """
+        Function to obtain images with person label (see helper.py) you want to use for processing or testing 
+
+        Note:
+            if obtaining rgb images be careful not to choose too many so you don't run out of memory
+
+        Args:
+            i_type: type of image (see helper.py). Default set to ImageAccess.ALL_TYPES
+            i_person: person you want (see helper.py). Default set to ImageAccess.ALL_PERSONS
+            i_degrees: image angle desired (see helper.py). Default set to ImageAccess.ALL_DEGREES
+            i_orientation: desired orientation (see helper.py). Default set to ImageAccess.ALL_PERSONS
+            i_color: image color scheme (see helper.py). Default set to ImageAccess.Grayscale
+
+        Returns:
+            np array of images filtered from data frame
+        """
+
+        current_df = ImageAccess._filter_df(i_type, i_person, i_degrees, i_orientation, i_color)
+
+        if i_color == Color.RGB:
+            images = current_df['RGB'].to_numpy()
+            labels = current_df['Person'].to_numpy()
+            return images, labels
+        else:
+            images = current_df['Grayscale'].to_numpy()
+            labels = current_df['Person'].to_numpy()
+            return images, labels
+    
--- src/module_examples.py
+++ src/module_examples.py
@@ -1,29 +1,31 @@
 import matplotlib.pyplot as plt
 import cv2
 
+from helper import *
 from image_access import ImageAccess
+
 
 """
 Module used to show use cases (examples) for different helper classes
 created
 """
 
-# ImageAccess Class use case
+### ImageAccess Class use case ###
 # Initially import ImageAccess from image_accesss module (see above)
 
 # only call function obtain_images: ImageAccess.obtain_images(i_type, i_person, i_degrees, i_orientation, i_color)
 
 # obtain all akshay training images as grayscale
-images_1 = ImageAccess.obtain_images(i_person=ImageAccess.AKSHAY)
+images_1 = ImageAccess.obtain_images(i_person=Label.AKSHAY)
 print(images_1.shape)
 # obtain all 30 degrees nabilah training images as grayscale
-images_2 = ImageAccess.obtain_images(i_person=ImageAccess.NABILAH, i_degrees=ImageAccess.DEG_30)
+images_2 = ImageAccess.obtain_images(i_person=Label.NABILAH, i_degrees=Angle.DEG_30)
 print(images_2.shape)
 # obtain all 45 degrees left oriented training images as grayscale
-images_3 = ImageAccess.obtain_images(i_degrees=ImageAccess.DEG_45, i_orientation=ImageAccess.LEFT)
+images_3 = ImageAccess.obtain_images(i_degrees=Angle.DEG_45, i_orientation=Orientation.LEFT)
 print(images_3.shape)
 # obtain all 0 degrees akshay images as rgb
-images_4 = ImageAccess.obtain_images(i_person=ImageAccess.AKSHAY, i_degrees=ImageAccess.DEG_0, i_color=ImageAccess.RGB)
+images_4 = ImageAccess.obtain_images(i_person=Label.AKSHAY, i_degrees=Angle.DEG_0, i_color=Color.RGB)
 print(images_4.shape)
 
 plt.imshow(images_1[0], cmap='gray')
@@ -34,3 +36,19 @@ plt.imshow(images_3[0], cmap='gray')
 plt.show()
 plt.imshow(images_4[0])
 plt.show()
+
+# obtain all training data with corresponding labels
+X_train, Y_train = ImageAccess.obtain_labeled_data() # everything default gives training images
+
+
+### helper module use case ###
+# initially import all from helper module (see above)
+
+# check accuracy of Y_train on Y_train (should give 100 % or 1)
+accuracy = get_accuracy(Y_train, Y_train)
+print(accuracy)
+
+# check confusion matrix of Y_train on Y_train (should give identity matrix)
+matrix = get_confusion_matrix(Y_train, Y_train)
+print(matrix)
+
